2b892260506a6d34f38e3909c2b893e5d491c0b1
Mchldv <michael_dave_02@yahoo.com>
2018-04-26 16:51:01 +0700

    copy folder resource in view/resources to assets

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -14,7 +14,7 @@ class Admin extends CI_Controller {
 
 	public function _example_output($output = null)
 	{
-		$this->load->view('home.php',(array)$output);
+		$this->load->view('index.php',(array)$output);
 	}
 
 	public function offices()
